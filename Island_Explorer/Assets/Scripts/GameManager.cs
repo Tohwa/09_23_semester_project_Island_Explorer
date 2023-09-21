172b0ac97adc83a402ab6f59444481761e77a958
Sascha <Tohwa.gaming@gmail.com>
2023-09-21 08:37:50 +0200

    created GameManager instance

--- Island_Explorer/Assets/Scripts/GameManager.cs
+++ Island_Explorer/Assets/Scripts/GameManager.cs
@@ -1,18 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    #region Fields
+    public static GameManager Instance { get; private set; }
+    #endregion
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(this);
     }
 }
